bd14e58252a7519c9ed81aff924fdba0794f76b5
erin cherney <erin.t.cherney@gmail.com>
2016-06-15 22:29:44 -0400

    rough rough portfolio

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  console.log("connected")
+
+
+
+$('#mailbutton').click(function(){
+  $('.modal').show();
+});
+$('#close').click(function(){
+  $('.modal').hide();
+});
+
+});
