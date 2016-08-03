b501835ce57f9c6d3a53b0ffebbda7e10db5cdc5
erin cherney <erin.t.cherney@gmail.com>
2016-08-02 21:58:53 -0400

    changing basically everything

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-$(document).ready(function() {
-  console.log("connected")
-
-
-
-$('#mailbutton').click(function(){
-  $('.modal').show();
-});
-$('#close').click(function(){
-  $('.modal').hide();
-});
-
-});
+console.log("connected")
