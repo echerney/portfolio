d301513327bea1b3cf6f76a5146ad0d01c385b41
erin cherney <erin.t.cherney@gmail.com>
2016-08-02 23:25:30 -0400

    restructured

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("connected")
+$(document).ready(function() {
+
+  console.log('joinin the rebellion!')
+
+  $('.bubble').click(changeClass)
+
+
+  function changeClass() {
+    $(this).toggleClass('bubble');
+    $(this).toggleClass('hovering');
+    let title = $(this).children()[0];
+    title.toggle();
+    let desc = $(this).children()[1];
+    desc.toggle();
+  }
+
+
+
+
+
+});
